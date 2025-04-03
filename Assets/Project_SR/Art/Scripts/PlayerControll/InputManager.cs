47f7083b88c2605eec44305b484614af2a3262fe
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-04 03:56:16 +0900

    플레이어 InputManager 작성

---
+++ Assets/Project_SR/Art/Scripts/PlayerControll/InputManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace SR
+{
+    public class InputManager : MonoBehaviour
+    {
+        public string horizontalInputName = "Horizontal";
+        public string verticalInputName = "Vertical";
+        public string mouseXInputName = "Mouse X";
+        public string mouseYInputName = "Mouse Y";
+        public string fireInputName = "Fire";
+        public string reloadInputName = "Reload";
+        public string jumpInputName = "Jump";
+
+        public Vector2 MoveInput { get; private set; }
+        public Vector2 LookInput { get; private set; }
+        public bool fire { get; private set; }
+        public bool reload { get; private set; }
+        public bool JumpPressed { get; private set; }
+
+        void Update()
+        {
+            MoveInput = new Vector2(Input.GetAxis(horizontalInputName), Input.GetAxis(verticalInputName));
+            LookInput = new Vector2(Input.GetAxis(mouseXInputName), Input.GetAxis(mouseYInputName));
+            fire = Input.GetButtonDown(fireInputName);
+            reload = Input.GetButtonDown(reloadInputName);
+            JumpPressed = Input.GetButtonDown(jumpInputName);
+        }
+    }
+}
+
